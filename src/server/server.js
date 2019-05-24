c22e89856ee7ac28d0f28938e70368708111b787
przekwas <luke@covalence.io>
2019-05-24 14:22:32 -0500

    adding useful express middleware

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,28 @@
 import path from 'path';
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
 import routes from './routes';
 
 const app = express();
 
+// Allows cross origin requests to our API
+// aka we can build a mobile app to talk to this server, too ;)
+app.use(cors());
+
+// Compresses gzips of requests and files for optimized speed!
+app.use(compression());
+
+// Terrific security middleware for common attacks
+app.use(helmet());
+
+// Serves static files from public folder
 app.use(express.static('public'));
+
+// Useful logging tool for all incoming requests
+app.use(morgan('dev'));
 
 app.use(routes);
 
