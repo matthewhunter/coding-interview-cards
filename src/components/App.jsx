b0923fda78b3abaeae882ac201dc3babda9461d9
Matthew Hunter <matthew.w.hunter@icloud.com>
2019-05-26 18:20:06 -0500

    deck holds cards, cards array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import Header from './Header'
-import Card from './Card'
+import Deck from './Deck'
 
 export default class App extends React.Component {
+  state = {
+    // just temporary values so I have something to iterate
+    cards: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+  }
   render () {
     const title = 'Coding Review Cards'
 
     return (
-      <div>
-        <Header title={ title } />
-        <Card />
-      </div>
+      <React.Fragment>
+        <Header title={title} />
+        <Deck cards={ this.state.cards } />
+      </React.Fragment>
     )
   }
 }
