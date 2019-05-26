4885d38429d3a1248bfeeede05a65c9fce0a9cf1
Matthew Hunter <matthew.w.hunter@icloud.com>
2019-05-26 09:56:34 -0500

    added Sass for styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
-const path = require('path');
-let nodeExternals = require('webpack-node-externals');
+const path = require('path')
+let nodeExternals = require('webpack-node-externals')
 
 const serverConfig = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: './src/server/server.js',
-    output: {
-        filename: 'server.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    target: 'node',
-    externals: [nodeExternals(), { knex: 'commonjs knex' }],
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/server/server.js',
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  target: 'node',
+  externals: [nodeExternals(), { knex: 'commonjs knex' }]
 }
 
 const clientConfig = {
-    mode: process.env.NODE_ENV || 'development',
-    entry: ['@babel/polyfill', './src/client/index.js'],
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'public/js')
-    }
+  mode: process.env.NODE_ENV || 'development',
+  entry: ['@babel/polyfill', './src/client/index.js'],
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public/js')
+  }
 }
 
-module.exports = [serverConfig, clientConfig];
+module.exports = [serverConfig, clientConfig]
