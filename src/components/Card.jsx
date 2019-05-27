6a2c49bd0f9b5163c3be4e5e408d1e9cbae55597
Matthew Hunter <matthew.w.hunter@icloud.com>
2019-05-26 23:23:13 -0500

    converted card to class

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 
-const Card = props => (
-  <div className='card' style={props.cardStyle}>
-    <div className='card-back' />
-    <div className='card-front' style={props.frontStyle}/>
-  </div>
-)
+export default class Card extends React.Component {
+  state = {
+    hover: 'card'
+  }
 
-export default Card
+  cardStyle = {
+    transform: `rotate(${Math.random() * 20 - 10}deg)`,
+    bottom: `${this.props.bottom}rem`
+  }
+  frontStyle = {
+    display: 'none'
+  }
+  onMouseOver = () => {
+    if (this.props.index === this.props.cards) {
+      this.setState(() => ({
+        hover: 'card lift'
+      }))
+    }
+  }
+  onMouseLeave = () => {
+    if (this.props.index === this.props.cards) {
+      this.setState(() => ({
+        hover: 'card'
+      }))
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hover} style={this.cardStyle} onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseLeave}>
+        <div className='card-back' />
+        <div className='card-front' style={this.frontStyle} />
+      </div>
+    )
+  }
+}
