ba0f8daf36f020481c1a22afe5707f81364e9b60
Matthew Hunter <matthew.w.hunter@icloud.com>
2019-05-29 16:26:05 -0500

    randomized card order, set up divs for play areas

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -2,22 +2,42 @@ import React from 'react'
 import cards from '../cards.js'
 import Card from './Card'
 
-let bottom = 1
+let bottom = 0.5
+
+const randomize = (array) => {
+  var index = array.length
+  var temporary
+  var random
+  while (0 !== index) {
+    random = Math.floor(Math.random() * index)
+    index -= 1
+    temporary = array[index]
+    array[index] = array[random]
+    array[random] = temporary
+  }
+  return array
+}
+
+randomize(cards)
 
 const Deck = props => (
   <React.Fragment>
-    {cards.map(card => {
-      let align = Math.random() * 20 - 10
-      let cardStyle = () => ({
-        transform: `rotate(${align}deg)`,
-        bottom: `${bottom}rem`
-      })
-      let frontStyle = {
-        display: 'none'
-      }
-      bottom = bottom + 1
-      return <Card key={card.key} index={card.key} cards={props.cards} card={card} cardStyle={cardStyle()} frontStyle={frontStyle} bottom={bottom} selectCard={props.selectCard} />
-    })}
+    <div className='draw'>
+      <p>Draw Pile</p>
+    </div>
+    <div className='playing'>
+      <p>Playing</p>
+    </div>
+    <div className='discard'>
+      <p>Discard Pile</p>
+    </div>
+    <div className='cards'>
+      {
+        cards.map( (card, index) => {
+        bottom = bottom + 0.5
+        return <Card key={card.key} index={index} cards={props.cards} card={card} bottom={bottom} selectCard={props.selectCard} />
+      })}
+    </div>
   </React.Fragment>
 )
 
