f01f5f21e744b3a9a126f13496c53c9a49515313
Matthew Hunter <matthew.w.hunter@icloud.com>
2019-05-29 16:22:27 -0500

    Removed grid as it isn't currently necessary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from './Header'
 import Deck from './Deck'
-import Main from './Main'
 import cards from '../cards.js'
 
 export default class App extends React.Component {
@@ -10,7 +9,7 @@ export default class App extends React.Component {
     currentCard: [],
     playedCards: []
   }
-  selectCard = (selectedCard) => {
+  selectCard = ( selectedCard ) => {
     this.state.currentCard.push(this.state.cards.pop())
     console.log( this.state.currentCard )
     this.setState( prevState => {this.state.cards.filter(card => card !== selectedCard)
@@ -22,16 +21,9 @@ export default class App extends React.Component {
 
     return (
       <React.Fragment>
-        <Header title={title} />
-        <div className='flex-grid'>
-          <div className='col'>
-            <Deck cards={this.state.cards} selectCard={this.selectCard} />
-          </div>
-          <div className='col-2'>
-            <Main />
-          </div>
-          <div className='col' />
-        </div>
+        {/* <Header title={title} /> */}
+          <Deck cards={this.state.cards} selectCard={this.selectCard} />
+
       </React.Fragment>
     )
   }
