5691517408179ac2118222199f7969501fb6205f
Matthew Hunter <matthew.w.hunter@icloud.com>
2019-05-29 16:25:18 -0500

    recreated cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,46 +2,68 @@ import React from 'react'
 
 export default class Card extends React.Component {
   state = {
-    hover: 'card'
+    containerClasses: 'card-container',
+    cardClasses: 'card',
+    style: {
+      transform: `rotate(${Math.random() * 20 - 10}deg)`,
+      bottom: `${this.props.index}vw`
+    }
   }
-  componentDidMount () {
-    this.props.cards.push( this )
+
+  rotation = Math.random() * 20 - 10
+  async componentDidMount() {
+    await this.props.cards.push(this)
     console.log(this.props)
   }
-  cardStyle = {
-    transform: `rotate(${Math.random() * 20 - 10}deg)`,
-    bottom: `${ this.props.bottom }rem`,
-    display: 'block'
-  }
-  frontStyle = {
-    display: 'none'
-  }
+
   onMouseOver = () => {
-    if (this.props.index === this.props.cards.length) {
-      this.setState(() => ({
-        hover: 'card lift'
-      }))
+    if (this.props.index !== this.props.cards.length - 1) {
+      return
     }
+    this.setState(() => ({
+      style: {
+        transform: 'unset'
+      },
+      cardClasses: 'card lift'
+    }))
   }
+
   onMouseLeave = () => {
-    if (this.props.index === this.props.cards.length) {
-      this.setState(() => ({
-        hover: 'card'
-      }))
+    if (this.props.index !== this.props.cards.length - 1) {
+      return
     }
+    this.setState(() => ({
+      style: {
+        transform: `rotate(${Math.random() * 20 - 10}deg)`
+      },
+      cardClasses: 'card'
+    }))
   }
-  onClick = (e) => {
-    if (this.props.index === this.props.cards.length) {
-      this.props.selectCard( this )
-      document.getElementById(`card-${this.props.index}`).remove()
+
+  onClick = async e => {
+    console.log(this.props.index, this.props.cards.length - 1)
+    if (this.props.index !== this.props.cards.length - 1) {
+      return
     }
+    await this.props.selectCard(this)
+    await console.log(this.props.cards)
+    await this.setState(() => ({
+      style: {
+        transform: `rotate(${Math.random() * 5 - 2.5}deg)`
+      },
+      cardClasses: 'card is-flipped'
+    }))
   }
 
   render() {
     return (
-      <div id={'card-' + this.props.index} className={ this.state.hover } style={ this.cardStyle } onMouseEnter={ this.onMouseOver } onMouseLeave={ this.onMouseLeave } onClick={(e) => this.onClick(e)}>
-        <div className='card-back' />
-        <div className='card-front' style={this.frontStyle} />
+      <div className={this.state.containerClasses} style={this.state.style} id={'card-' + this.props.index} onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseLeave} onClick={e => this.onClick(e)}>
+        <div className={this.state.cardClasses}>
+          <div className='card-face card-back' />
+          <div className='card-face card-front'>
+            <p>{this.props.card.challenge}</p>
+          </div>
+        </div>
       </div>
     )
   }
