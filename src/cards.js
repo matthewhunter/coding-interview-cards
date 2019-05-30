cc5150af26e57c8d58b3158efe5a6babab8fc18c
Matthew Hunter <matthew.w.hunter@icloud.com>
2019-05-30 09:42:06 -0500

    added additional example questions

--- src/cards.js
+++ src/cards.js
@@ -1,53 +1,143 @@
 const cards = [
   {
-    key: 1,
-    challenge: 'Explain event delegation',
-    answer: 'Answer goes here.'
+    id: 1,
+    challenge: 'Explain event delegation.',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 2,
-    challenge: 'Explain how this works in JavaScript',
-    answer: 'Answer goes here.'
+    id: 2,
+    challenge: "Explain how 'this' works in JavaScript.",
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 3,
-    challenge: 'Explain how prototypal inheritance works',
-    answer: 'Answer goes here.'
+    id: 3,
+    challenge: 'Explain how prototypal inheritance works.',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 4,
+    id: 4,
     challenge: 'What is a closure, and how/why would you use one?',
-    answer: 'Answer goes here.'
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 5,
-    challenge: 'Can you describe the main difference between a .forEach loop and a .map() loop and why you would pick one versus the other?',
-    answer: 'Answer goes here.'
+    id: 5,
+    challenge: "What's the difference between a forEach loop and a map loop? Why you would pick one over the other?",
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 6,
+    id: 6,
     challenge: 'What is a typical use case for anonymous functions?',
-    answer: 'Answer goes here.'
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 7,
+    id: 7,
     challenge: 'What is the difference between host objects and native objects?',
-    answer: 'Answer goes here.'
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 8,
+    id: 8,
     challenge: 'When would you use document.write()?',
-    answer: 'Answer goes here.'
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 9,
-    challenge: 'Explain Ajax in as much detail as possible.',
-    answer: 'Answer goes here.'
+    id: 9,
+    challenge: 'Explain AJAX in as much detail as possible.',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   },
   {
-    key: 10,
+    id: 10,
     challenge: 'Explain "hoisting".',
-    answer: 'Answer goes here.'
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 11,
+    challenge: 'What is Coercion?',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 12,
+    challenge: "What is the 'typeof' operator?",
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 13,
+    challenge: 'What is an object?',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 14,
+    challenge: 'Explain arrays in JavaScript.',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 15,
+    challenge: 'Explain the different types of equality in JavaScript.',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 16,
+    challenge: 'Explain how scope works in JavaScript.',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 17,
+    challenge: "How do 'let' and 'var' differ?",
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 18,
+    challenge: "What is the difference between 'null' and 'undefined?'",
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 19,
+    challenge: 'What is functional programming?',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
+  },
+  {
+    id: 20,
+    challenge: 'What is Big O notation, and why is it useful?',
+    answer: 'Answer goes here.',
+    resource: '',
+    video: ''
   }
 ]
 
